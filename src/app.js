515ae0c12ab94dabb6f9ca17723cb0df1fbe2f3d
Akshay More <AkshayM@proteantech.in>
2024-06-18 17:47:37 +0530

    user controller and route

--- src/app.js
+++ src/app.js
@@ -10,4 +10,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// routes import
+import userRouter from './routes/user.routes.js
+
+// routes implementation
+app.use('/pi/v1/users', userRouter)
+
 export { app };
--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -1,5 +1,6 @@
 import multer from "multer";
 
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/temp");
@@ -8,6 +9,7 @@ const storage = multer.diskStorage({
     cb(null, file.originalname);
   },
 });
+
 
 export const upload = multer({
   storage,
